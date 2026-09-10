2df3126f0acfa0baf9c22b12b2c10c0ac0c0fdf7
uditajain29-debug <uditajain29@gmail.com>
2026-09-10 14:53:16 +0530

    Add files via upload

---
+++ day1 b.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+printf("enter the numbers: ");
+scanf("%d %d",&a,&b);
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+printf("sum=%d\n",c);
+printf("difference=%d\n",d);
+printf("product=%d\n",e);
+printf("quotient=%d\n",f);
+return 0;
+}
+
