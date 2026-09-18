60142315574dbcee9b6fb7ba0427e307c83f6f04
uditajain29-debug <uditajain29@gmail.com>
2026-09-18 21:18:50 +0530

    Add files via upload

---
+++ day 25/day 25b.c
@@ -0,0 +1,20 @@
+// Q50 Write a program to print the following pattern:
+/*
+*****
+ ****
+  ***
+   **
+    *
+    */
+   #include <stdio.h>
+int main()
+{
+    for(int i=5;i>0;i--)
+    {
+        for(int j=0;j<i;j++)
+        {
+            printf("*");
+        }
+printf("\n");
+    }
+}
